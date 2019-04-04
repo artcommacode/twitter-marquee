a4a1e04177a2d3b4f98fb9d7953afa1ce177fbe0
Ryan <ryan@artcommaco.de>
2019-04-04 21:28:51 +0100

    lets get this compiling

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,34 @@ dotenv.config()
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
-const client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-})
+if (
+  process.env.CONSUMER_KEY &&
+  process.env.CONSUMER_SECRET &&
+  process.env.ACCESS_TOKEN_KEY &&
+  process.env.ACCESS_TOKEN_SECRET
+) {
+  const client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  })
 
-const title = `I thought what I'd do was, I'd pretend to be one of those deaf-mutes. `
+  const title = `I thought what I'd do was, I'd pretend to be one of those deaf-mutes. `
 
-const makeName = async (index: number) => {
-  const name = title
-    .slice(index)
-    .concat(title)
-    .slice(0, 50)
-  try {
-    await client.post('account/update_profile', {name})
-  } catch (error) {
-    console.log(error)
+  const makeName = async (index: number) => {
+    const name = title
+      .slice(index)
+      .concat(title)
+      .slice(0, 50)
+    try {
+      await client.post('account/update_profile', {name})
+    } catch (error) {
+      console.log(error)
+    }
+    await sleep(10 * 60 * 1000)
+    makeName((index + 1) % 50)
   }
-  await sleep(10 * 60 * 1000)
-  makeName((index + 1) % 50)
-}
 
-makeName(0)
+  makeName(0)
+}
