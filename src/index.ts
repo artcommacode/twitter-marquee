66d02ef064f9c0cc50d66c228400a3d8926e51c9
Ryan <ryan@artcommaco.de>
2019-04-04 20:40:42 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as Twitter from 'twitter'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
+const title = `I thought what I'd do was, I'd pretend to be one of those deaf-mutes. `
+
+const makeName = async (index: number) => {
+  const name = title
+    .slice(index)
+    .concat(title)
+    .slice(0, 50)
+  try {
+    await client.post('account/update_profile', {name})
+  } catch (error) {
+    console.log(error)
+  }
+  await sleep(10 * 60 * 1000)
+  makeName((index + 1) % 50)
+}
+
+makeName(0)
