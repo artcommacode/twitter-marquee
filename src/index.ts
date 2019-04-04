c59f866aa066f14c307162d95663ac5ed06fe976
Ryan <ryan@artcommaco.de>
2019-04-04 21:56:00 +0100

    better error reporting

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,36 @@ dotenv.config()
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 if (
-  process.env.CONSUMER_KEY &&
-  process.env.CONSUMER_SECRET &&
-  process.env.ACCESS_TOKEN_KEY &&
-  process.env.ACCESS_TOKEN_SECRET
+  !process.env.CONSUMER_KEY ||
+  !process.env.CONSUMER_SECRET ||
+  !process.env.ACCESS_TOKEN_KEY ||
+  !process.env.ACCESS_TOKEN_SECRET
 ) {
-  const client = new Twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-  })
-
-  const title = `I thought what I'd do was, I'd pretend to be one of those deaf-mutes. `
-
-  const makeName = async (index: number) => {
-    const name = title
-      .slice(index)
-      .concat(title)
-      .slice(0, 50)
-    try {
-      await client.post('account/update_profile', {name})
-      console.log(name)
-    } catch (error) {
-      console.log(error)
-    }
-    await sleep(10 * 60 * 1000)
-    makeName((index + 1) % 50)
-  }
-
-  makeName(0)
+  throw new Error('Must set all environment varialbles')
 }
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
+const title = `I thought what I'd do was, I'd pretend to be one of those deaf-mutes. `
+
+const makeName = async (index: number) => {
+  const name = title
+    .slice(index)
+    .concat(title)
+    .slice(0, 50)
+  try {
+    await client.post('account/update_profile', {name})
+    console.log(name)
+  } catch (error) {
+    console.log(error)
+  }
+  await sleep(10 * 60 * 1000)
+  makeName((index + 1) % 50)
+}
+
+makeName(0)
